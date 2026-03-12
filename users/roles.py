89e5600d4de35ebea47c92f4704ef460e5a4827d
Antonio Gabriel <biepe07@gmail.com>
2026-03-12 15:38:21 -0300

    Criando as permissoes

---
+++ users/roles.py
@@ -0,0 +1,9 @@
+from rolepermissions.roles import AbstractUserRole
+
+class Trainee(AbstractUserRole):
+    available_permissions = {'view_workout': True}
+
+class Trainer(AbstractUserRole):
+    available_permissions = {
+        'view_workout': True, 'create_workout': True, 'edit_workout': True, 'delete_workout': True, 'create_trainee': True
+    }
