1c1e59eba5bf49627470f22a17741a4eef17c62b
Antonio Gabriel <biepe07@gmail.com>
2026-03-12 15:28:02 -0300

    Criando modelo de usuario

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(primary_key=True, null=False)
+    name = models.CharField(max_length=100, null=False)
+    phone = models.CharField(max_length=15, null=False)
