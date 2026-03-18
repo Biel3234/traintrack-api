93179ee6ec136a57d72609981bdbb061e7831743
Antonio Gabriel <biepe07@gmail.com>
2026-03-18 18:35:19 -0300

    Criando serializers

---
+++ workouts/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Exercise, Workout, WorkoutExercise
+
+class ExerciserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exercise
+
+        fields = ['name']
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workout
+
+        fields = ['__all__']
+
+class WorkoutExerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkoutExercise
+
+        fields = ['__all__']
